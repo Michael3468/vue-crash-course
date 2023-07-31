1d132b8c037057e2158f57f0fed891bb91d3706a
Mikhail <mikhail3468@yandex.ru>
2023-07-31 18:33:14 +0300

    recreate app with vue cli

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <NavBar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+  ></NavBar>
+
+  <PageViewer :page="pages[activePage]"></PageViewer>
+</template>
+
+<script>
+import NavBar from './components/NavBar';
+import PageViewer from './components/PageViewer';
+
+export default {
+  components: {
+    NavBar,
+    PageViewer,
+  },
+  data() {
+    return {
+      activePage: 0,
+      pages: [
+        {
+          link: { text: 'Home', url: 'index.html' },
+          pageTitle: 'Home page',
+          content: 'This is the Home content',
+        },
+        {
+          link: { text: 'About', url: 'about.html' },
+          pageTitle: 'About page',
+          content: 'This is the About content',
+        },
+        {
+          link: { text: 'Contact', url: 'contact.html' },
+          pageTitle: 'Contact page',
+          content: 'This is the Contact content',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav :class="[`navbar-${theme} bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand">My Vue</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item" v-for="(page, index) in pages" :key="index">
+          <a
+            class="nav-link"
+            :class="{ active: activePage === index }"
+            aria-current="page"
+            :href="page.link.url"
+            :title="`This link goes to the ${page.link.text} page`"
+            @click.prevent="navLinkClick(index)"
+          >
+            {{ page.link.text }}
+          </a>
+        </li>
+      </ul>
+
+      <form class="d-flex">
+        <button class="btn btn-primary" @click.prevent="changeTheme()">
+          Toggle Dark Mode
+        </button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["pages", "activePage", "navLinkClick"],
+
+  data() {
+    return {
+      theme: "light",
+    };
+  },
+
+  methods: {
+    changeTheme() {
+      this.theme === "light" ? (this.theme = "dark") : (this.theme = "light");
+    },
+  },
+};
+</script>
